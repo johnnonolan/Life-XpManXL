28a9d847394e65b1da4c52f8999bd7b061d2ca3e
Johnno Nolan <johnnonolan@yahoo.co.uk>
2011-01-20 22:16:06 +0000

    JimBob's and my ...err attempt at Life

---
+++ ConwaysGameOfLife/Grid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Grid
+    {
+        public override bool Equals(object obj)
+        {
+
+            return obj != null && this.GetHashCode() == obj.GetHashCode();
+
+        }
+
+        public override int GetHashCode()
+        {
+            return "".GetHashCode();
+        }
+    }
+}
